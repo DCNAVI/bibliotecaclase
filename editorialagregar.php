aa1216fbb054d12eed339f39497f63a2a8cc7e5e
DCNAVI <icarapia94@gmail.com>
2017-02-23 16:17:28 -0600

    base

---
+++ editorialagregar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>editoral agregar</title>
+</head>
+<body>
+<form action = "#" method = "POST">
+    EDITORIAL:
+   <input  type = "text" name = "editorial" placeholder = "editorial"  required style="WIDTH: 400px; HEIGHT: 20px"  ><br /><br/>
+
+   
+
+<br/>
+
+
+         <input type = "submit" name = "btnregistro" value = "Registrar " style='width:160px; height:35px' ><br/>
+               
+                </form> <!--fin form -->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>mi primer pagina web</title>
+</head>
+<body>
+este el cuerpo de la pagina 
+</body>
+</html>
